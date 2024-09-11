1a128edf321a8feb6aea8b0b208f4cf98adbdc1b
Animesh Thomas <animeshthomas262@gmail.com>
2024-09-11 19:25:43 +0530

    Create Rectangle.py

---
+++ Rectangle.py
@@ -0,0 +1,13 @@
+class Rectangle:
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+    def __iter__(self):
+        yield {'length': self.length}
+        yield {'width': self.width}
+
+rect = Rectangle(5, 3)
+
+for item in rect:
+    print(item)
