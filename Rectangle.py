f80e1aa6da161b8504d9f06bcef6f6328c0cbb65
Animesh Thomas <animeshthomas262@gmail.com>
2024-09-11 19:27:08 +0530

    Update Rectangle.py

--- Rectangle.py
+++ Rectangle.py
@@ -6,7 +6,7 @@ class Rectangle:
     def __iter__(self):
         yield {'length': self.length}
         yield {'width': self.width}
-
+#creating rect obj
 rect = Rectangle(5, 3)
 
 for item in rect:
